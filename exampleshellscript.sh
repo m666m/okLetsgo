49fb220d834d4b2e4e4a942b90fedfc00c34835b
m666m <m666m@github.com>
2020-07-24 11:30:31 +0800

    常用脚本语句

---
+++ exampleshellscript.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#
+# 参考:
+# https://zhuanlan.zhihu.com/p/123989641
+
+# -x ： 在执行每一个命令之前把经过变量展开之后的命令打印出来。
+# -e ： 遇到一个命令失败（返回码非零）时，立即退出。
+# -u ：试图使用未定义的变量，就立即退出。
+# -o pipefail ： 只要管道中的一个子命令失败，整个管道命令就失败。
+set -xeuo pipefail
+
+# 防止重叠运行
+exec 123<>lock_myscript   # 把lock_myscript打开为文件描述符123
+flock  --wait 5  123 || { echo 'cannot get lock, exit'; exit 1; }
+
+# 意外退出时杀掉所有子进程
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+
+# timeout 限制运行时间
+timeout 600s  some_command arg1 arg2
+
+# 连续管道时，考虑使用 tee 将中间结果落盘，以便查问题
+cmd1 | tee out1.dat | cmd2 | tee out2.dat | cmd3 > out3.dat
+
+# 字符串带颜色打印出来
+_green() {
+    printf '\033[1;31;32m'
+    printf -- "%b" "$1"
+    printf '\033[0m'
+}
+
+_red() {
+    printf '\033[1;31;31m'
+    printf -- "%b" "$1"
+    printf '\033[0m'
+}
+
+_yellow() {
+    printf '\033[1;31;33m'
+    printf -- "%b" "$1"
+    printf '\033[0m'
+}
+
+# test:
+# xxx="xyz"
+# _red "abcde"
+# _yellow $xxx
+
