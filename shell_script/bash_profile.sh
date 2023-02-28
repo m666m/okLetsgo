5e37dcf7f92f54f378b9962e8842a5e0d072add6
m666m <31643783+m666m@users.noreply.github.com>
2023-03-01 01:03:00 +0800

    ryi7jhe64swdfg

--- shell_script/bash_profile.sh
+++ shell_script/bash_profile.sh
@@ -49,13 +49,15 @@ if [ -x /usr/bin/dircolors ]; then
     # git 常用命令
     alias gs='echo "git status ..." && git status'
     alias glog='echo "[树形提交记录]" && git log --oneline --graph'
+    alias glm='echo "[本地远程库对比本地库--master]" && git log --graph --oneline ..origin/master'
+    alias gld='echo "[本地远程库对比本地库--dev]" && git log --graph --oneline ..origin/dev --'
     alias gdh='echo "[对比最近的两次提交]" && git diff HEAD^ HEAD'
     # git 经常断连，自动重试直至成功
     alias gpull='git pull || while (($? != 0)); do   echo -e "[Retry pull...] \n" && sleep 1; git pull; done'
     alias gpush='git push || while (($? != 0)); do   echo -e "[Retry push...] \n" && sleep 1; git push; done'
 
     # gpg 常用命令
-    alias gkey='echo "[有私钥的gpg密钥]" && gpg -K --keyid-format=long'
+    alias pkey='echo "[有私钥的gpg密钥]" && gpg -K --keyid-format=long'
 fi
 
 ####################################################################
