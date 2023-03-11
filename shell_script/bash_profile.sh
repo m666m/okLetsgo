f004ad5c1562b43a6c135697a059a87343949fd9
m666m <31643783+m666m@users.noreply.github.com>
2023-03-11 18:47:01 +0800

    hmswrgvut.nb

--- shell_script/bash_profile.sh
+++ shell_script/bash_profile.sh
@@ -53,12 +53,15 @@ if [ -x /usr/bin/dircolors ]; then
     alias pkey='echo "[有私钥的gpg密钥]" && gpg -K --keyid-format=long'
 
     # git 常用命令
-    alias gs='echo "git status ..." && git status'
+    alias gs='echo "git status:" && git status'
+    alias gdf='echo "git diff:" && git diff HEAD^ HEAD'
+    alias gds='echo "[差异：比较暂存区(staged)和仓库(HEAD)]" && git diff --staged'
+    alias gdh='echo "[差异：比较最近的两次提交记录]" && git diff HEAD^ HEAD'
     alias glog='echo "[提交记录：树形]" && git log --oneline --graph'
     alias glm='echo "[提交记录：本地远程库对比本地库--master]" && git log --graph --oneline ..origin/master --'
     alias gld='echo "[提交记录：本地远程库对比本地库--dev]" && git log --graph --oneline ..origin/dev --'
-    alias gdh='echo "[差异：对比最近的两次提交]" && git diff HEAD^ HEAD'
-    alias gba='echo "[分支：全部分支带最近提交的注释]" && git branch -avv'
+    alias gba='echo "[分支：全部分支带最近提交及注释]" && git branch -avv'
+
     # git 经常断连，自动重试直至成功
     alias gpull='git pull || while (($? != 0)); do   echo -e "[Retry pull...] \n" && sleep 1; git pull; done'
     alias gpush='git push || while (($? != 0)); do   echo -e "[Retry push...] \n" && sleep 1; git push; done'
