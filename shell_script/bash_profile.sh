22113f6720046717e7f4f4eef4277fd1b0acb0c9
m666m <31643783+m666m@users.noreply.github.com>
2023-04-19 22:25:45 +0800

    6w4je6ue

--- shell_script/bash_profile.sh
+++ shell_script/bash_profile.sh
@@ -26,7 +26,7 @@ export TERM=xterm-256color
 # alias 本该放到 .bashrc 文件，为了方便统一在此了
 # 可直接把 alias 放到 .zshrc 中保持自己的使用习惯
 #
-# 参考自 dbian 的 .bashrc 脚本中，常用命令开启彩色选项
+# 参考自 Debian 的 .bashrc 脚本中，常用命令开启彩色选项
 # enable color support of ls and also add handy aliases
 # 整体仍然受终端模拟器对16种基本颜色的设置控制，也就是说，在终端模拟器中使用颜色方案，配套修改 dir_colors ，让更多的文件类型使用彩色显示
 # curl -fsSLo ~/.dir_colors https://github.com/arcticicestudio/nord-dircolors/raw/develop/src/dir_colors
