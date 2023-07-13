4be227fc4599a855dc1f688f8371cd662a1d0a92
m666m <31643783+m666m@users.noreply.github.com>
2023-07-14 01:42:30 +0800

    5wj7ksshfbs

--- shell_script/bash_profile.sh
+++ shell_script/bash_profile.sh
@@ -388,12 +388,13 @@ function PS1git-branch-name {
         #
         #   如果使用git for Windows自带的mintty bash，它自带git状态脚本(貌似Debian系的bash都有)
         #   只要启动bash ，其会自动 source C:\Program Files\Git\etc\profile.d\git-prompt.sh，
-        #   最终执行 C:\Program Files\Git\mingw64\share\git\completion\git-prompt.sh。
+        #   最终 source C:\Program Files\Git\mingw64\share\git\completion\git-prompt.sh。
         #   这样实现了用户 cd 到git管理的目录就会显示git状态字符串。
         #   来源 https://github.com/git/git/blob/master/contrib/completion/git-prompt.sh
         #   如果自定义命令提示符，可以在PS1变量拼接中调用函数 $(__git_ps1 "(%s)") ，
         #   可惜tag和hashid的提示符有点丑，为了显示速度快，忍忍得了
-        #
+        #   或可考虑换用 https://github.com/magicmonty/bash-git-prompt
+
         # __git_ps1 居然透传 $?，前面的命令执行结果被它作为返回值了，只能先清一下，佛了
         _pp_git_pt=$(>/dev/null; __git_ps1 '%s' 2>/dev/null)
         if [ "$?" = "0" ]; then
