bee6365358e5c7949e640607b3f21c74e843486c
m666m <31643783+m666m@users.noreply.github.com>
2025-03-29 11:05:27 +0800

    myts75wdjj

--- bash_profile.sh
+++ bash_profile.sh
@@ -712,10 +712,6 @@ function PS1virtualenv-env-name {
 
 function PS1git-branch-name {
 
-    if ! command -v git >/dev/null 2>&1; then
-        return
-    fi
-
     # 优先使用 __git_ps1() 取分支名信息，如果取到有效的 git 信息，就直接打印出来，然后退出函数
     #
     #   如果使用git for Windows自带的mintty bash，它自带git状态脚本(貌似Debian系的bash都有)
@@ -741,6 +737,9 @@ function PS1git-branch-name {
     fi
 
     # __git_ps1 没取到有效信息，则自行获取git信息
+    if ! command -v git >/dev/null 2>&1; then
+        return
+    fi
 
     # 一条命令取当前分支名
     # 命令 git symbolic-ref 在裸仓库或 .git 目录中运行不报错，都会打印出当前分支名，
