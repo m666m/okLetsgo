8b8f4b7fc9db31c6e0b195844bd6539e4d1e96d4
m666m <31643783+m666m@users.noreply.github.com>
2026-02-03 23:01:45 +0800

    98g43w5

--- bash_profile.sh
+++ bash_profile.sh
@@ -567,8 +567,8 @@ function pdmsd() {
     echo "[podman 列出镜像的摘要，需要完整的镜像地址]"
     skopeo inspect --format "{{.Digest}}" docker://${1}
 }
-alias pdmrun='echo "[podman简单运行一个容器]"; podman run -it --rm -P'
-alias pdme='echo "[podman在运行的容器里执行一个命令]"; podman exec'
+alias pdmc='echo "[podman简单运行一个容器]"; podman run -it --rm -P'
+alias pdmexec='echo "[podman在运行的容器名里执行shell命令]"; podman exec'
 function pdmtty() {
     echo "[登录到容器 $1 内的tty]"
     podman exec -it $1 sh
@@ -576,36 +576,16 @@ function pdmtty() {
 alias pdmip='echo "[podman列出所有容器的ip和开放端口(rootless容器无ip地址)]"; podman inspect -f="{{.Name}} {{.NetworkSettings.IPAddress}} {{.HostConfig.PortBindings}}" $(podman ps -aq)'
 alias pdmlog='echo "[podman查看指定容器日志]"; podman logs -f --tail 100'
 alias pdmdf='echo "[podman查看资源情况]"; podman system df -v'
-alias pdmvp='echo "[podman清理空闲空间]"; podman volume prune'
 #
 export PDM_LOCAL_REPO="192.168.0.111:5000" && echo "podman 本地私有仓库地址设置为 PDM_LOCAL_REPO=${PDM_LOCAL_REPO}"
-alias pdmrs='echo "[podman 搜索包含本地无tls私有仓库]"; podman search --tls-verify=false'
 alias pdmr='echo "[podman 列出本地私有仓库 ${PDM_LOCAL_REPO} 的所有镜像]"; curl http://${PDM_LOCAL_REPO}/v2/_catalog'
-function pdmrtag() {
-    echo "[podman 列出本地私有仓库 ${PDM_LOCAL_REPO} 镜像 ${1} 的所有 tag]"
-    local img=$(echo $1  |cut -d: -f1)
-    curl http://${PDM_LOCAL_REPO}/v2/${img}/tags/list
-}
-function pdmrm() {
+function pdmrs() {
     echo "[podman 显示本地私有仓库 ${PDM_LOCAL_REPO} 镜像 ${1} 的 manifests]"
     local img=$(echo $1  |cut -d: -f1)
     local tag=$(echo $1  |cut -d: -f2)
     curl http://${PDM_LOCAL_REPO}/v2/${img}/manifests/${tag}
 }
-function pdmrt() {
-    local img=$(basename ${1})
-    echo "[podman 给本地镜像 ${1} 打标签为私有仓库 ${PDM_LOCAL_REPO}/$img]"
-    podman tag $1 ${PDM_LOCAL_REPO}/$img
-}
-function pdmrh() {
-    echo "[podman 向本地私有仓库推送镜像 ${PDM_LOCAL_REPO}/$1]"
-    podman push --tls-verify=false ${PDM_LOCAL_REPO}/$1
-}
-function pdmrl() {
-    echo "[podman 从本地私有仓库拉取镜像 ${PDM_LOCAL_REPO}/$1]"
-    podman pull --tls-verify=false ${PDM_LOCAL_REPO}/$1
-}
-function pdmrd() {
+function pdmrm() {
     local img=$(echo $1  |cut -d: -f1)
     local tag=$(echo $1  |cut -d: -f2)
     local sha=$2
