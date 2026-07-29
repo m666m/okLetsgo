94f5d6894a1d57770038bbc550ad66d0fa3a38f2
m666m <31643783+m666m@users.noreply.github.com>
2026-07-29 17:02:46 +0800

    2o04hggh[09645=

--- bash_profile.sh
+++ bash_profile.sh
@@ -141,9 +141,6 @@ export PDMREPO="192.168.0.111:5000" && echo "内网私有容器镜像仓库地�
 poor_connection() {
     # huggingface.co
     export HF_ENDPOINT=https://hf-mirror.com
-    # hf-mirror.com 可能没有代理 Xet 的 CAS（内容寻址存储）服务器，
-    # 导致下载时请求 cas-server.xethub.hf.co 返回了 401 Unauthorized
-    export HF_HUB_DISABLE_XET=1
 
     # uv
     export UV_DEFAULT_INDEX=https://pypi.tuna.tsinghua.edu.cn/simple/
@@ -1109,6 +1106,10 @@ ghaddr() {
 
     rm "$tfile"
 }
+
+# hf.co 镜像站 hf-mirror.com 可能没有代理 Xet 的 CAS（内容寻址存储）服务器，
+# 导致下载时请求 cas-server.xethub.hf.co 返回了 401 Unauthorized
+alias hfs='echo "[hf 命令关闭 https 下载 hf-mirror.com 的内容]" >&2; HF_HUB_DISABLE_XET=1 hf'
 
 # gpg 常用命令，一般用法都是后跟文件名即可
 alias ggk='echo "[查看有私钥的gpg密钥及其子密钥，带指纹和keygrip]" >&2; gpg -K --keyid-format=long --with-subkey-fingerprint --with-keygrip'
